6049a2d407667e29df8b9384fcfcf51a001ed178
Mwenje <mwenje24@gmail.com>
2022-06-14 06:21:08 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener( "DOMContentLoaded", function () {
+    changeText()
+  } );
+  
+  function changeText() {
+    const pText = document.getElementById( "text" )
+      pText.innerHTML = "This is really cool!";
+  }
